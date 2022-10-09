a51b104c8da2c66b5ea1d7c5779fbcda2eeb4cdd
Prashant Singh <octanesingh@gmail.com>
2022-10-09 20:18:28 +0530

    Basic homescreen layout created

--- src/components/Docker.tsx
+++ src/components/Docker.tsx
@@ -13,11 +13,17 @@ type DockerProps = {
 const Docker: React.FunctionComponent<DockerProps> = (props) => {
   const [isMouseIn, setIsMouseIn] = useState(false);
   let dockerStyles: SerializedStyles = css`
-    background: black;
+    background: rgba(255, 255, 255, 0.05);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(14.5px);
+    -webkit-backdrop-filter: blur(14.5px);
+    border-radius: 10px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
     width: 80vw;
-    height: 10vh;
+    height: 100%;
     transition: 0.2s;
-    ${isMouseIn ? `transform:scale(1.2)` : ``}
+    z-index: 1000;
+    ${isMouseIn ? `transform:scale(1.1)` : ``}
   `;
   return (
     <div
