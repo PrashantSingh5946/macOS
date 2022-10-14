2071268b36d4681b93cb9c9b504b020ebc643a7d
Prashant Singh <octanesingh@gmail.com>
2022-10-14 19:33:52 +0530

    Adding the theme reducer

---
+++ src/store/reducers/theme.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+enum theme {
+    dark="dark",
+    light="light",
+    glow="glow"
+}
+
+
+
+
+export const themeSlice = createSlice({
+    name: 'theme',
+    initialState: {
+        currentTheme: theme.light,
+    },
+    reducers: {
+        setDarkTheme: (state) => { state.currentTheme = theme.dark},
+        setLightTheme: (state) => { state.currentTheme = theme.light},
+    },
+})
+
+export const { setDarkTheme, setLightTheme } = themeSlice.actions
+
+export default themeSlice.reducer
