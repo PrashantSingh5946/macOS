041da5d3690a2d961a55e0df79f6f7b18fda3c6d
Prashant Singh <octanesingh@gmail.com>
2022-10-28 23:35:02 +0530

    Adding timestamp

---
+++ src/components/DateStamp.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const DateStamp = () =>
+{
+    const [date,setDate] = useState<Date>();
+
+    useEffect(()=>{
+        let date = new Date();
+        setDate(date);
+        let identifier = setInterval(()=>{
+            setDate(new Date());
+        },1000);
+
+        return ()=>{
+            clearInterval(identifier);
+        }
+    },[]);
+    
+    
+
+    return(
+        <div>
+            {
+                date?.toDateString()
+            }
+        </div>
+    )
+}
+
+export default DateStamp;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,22 @@ import { css } from "@emotion/react";
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
+import DateStamp from "./DateStamp";
 
 const Navbar = () => {
   let theme = useSelector<RootState>((state) => state.theme.currentTheme);
   let styles = css`
     ${theme === "light"
-      ? `background: rgba(255,255,255,0.25);`
-      : `background:rgba(0,0,0,0.25);`}
+      ? `color:black;background: rgba(255,255,255,0.25);`
+      : `color:white;background:rgba(0,0,0,0.25);`}
     backdrop-filter:blur(10px);
     height: 1.8rem;
     display: flex;
     align-items: center;
+
   `;
 
-  return <div css={styles}>{new Date().toString()}</div>;
+  return <div css={styles}>{<DateStamp/>}</div>;
 };
 
 export default Navbar;
