4b0e47542148d0b72b360da6b879f204482c11a3
prashant singh <prashant.singh@softway.com>
2023-06-17 13:05:17 +0530

    Spotify plugin complete

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
@@ -11,4 +12,18 @@ export default defineConfig({
       },
     }),
   ],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+
 });
