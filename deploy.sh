cfbae6fe463ed935ffd918921566088e24ce6326
Prashant Singh <octanesingh@gmail.com>
2023-10-15 08:30:02 +0530

    Adding the deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#Run npm install for the frontend
+npm install
+
+#Run npm install for the server
+cd server
+npm install
+cd ..
+
+#Install vite globally
+npm i -g vite
+
+#Run the vite build command
+vite build --force
+
+#Move the vite bundled files to server
+ mv dist server/
